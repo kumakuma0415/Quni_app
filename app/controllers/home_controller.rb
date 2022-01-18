487755d17c298e357d498edcd76142c211a099ad
kuma.seike <0k4u1m5a@gmail.com>
2022-01-18 17:24:51 +0900

    2th-commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def top
     @posts = Post.all
+
+    agent = Mechanize.new
+    page = agent.get("https://freesworder.net")
+    @elements = page.search('.entry-title')
   end
 
   def login
@@ -20,6 +24,9 @@ class HomeController < ApplicationController
   end
 
   def cafe
+    agent = Mechanize.new
+    page = agent.get("http://www.coop.kyushu-u.ac.jp/teishoku220117.html")
+    @elements = page.search('.menu')
   end
 
   def shop
